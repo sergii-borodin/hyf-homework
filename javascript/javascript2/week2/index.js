21944d2ff3ea0b554a63aebe9fba8598782bdd11
BoroDinS <sergey.borodin.lg@gmail.com>
2022-02-03 22:05:38 +0100

    Working with movies done

--- javascript/javascript2/week2/index.js
+++ javascript/javascript2/week2/index.js
@@ -45,4 +45,82 @@ console.log(
 
 // 4. Create a new array that has an extra key called tag.
 // The tag is based on the rating: Good(>= 7), Average(>= 4 and < 7), Bad(< 4)
-const arrMoviesWithTag = movies.map((movie) => movie);
+
+const newMoviesWithRatingTag = movies.map((movie) => {
+  const isGood = movie.rating >= 7;
+  const isAverage = movie.rating >= 4;
+  const isBad = movie.rating < 4;
+  if (isGood) {
+    movie.tag = "Good";
+  } else if (isAverage) {
+    movie.tag = "Average";
+  } else if (isBad) {
+    movie.tag = "Bad";
+  }
+  return movie;
+});
+
+console.log("newMoviesWithRatingTag", newMoviesWithRatingTag);
+
+// 5. Using chaining, first filter the movies array to only contain the movies rated higher than 6.
+// Now map the movies array to only the rating of the movies.
+
+const moviesWithRatingOver6 = movies
+  .filter((movie) => movie.rating > 6)
+  .map((movie) => movie.rating);
+
+console.log("moviesWithRatingOver6", moviesWithRatingOver6);
+
+// 6. Count the total number of movies containing any of following keywords: Surfer, Alien or Benjamin.
+// So if there were 3 movies that contained Surfer, 1 with Alien and 2 with Benjamin,
+// you would return 6.
+//  Can you make sure the search is case insensitive ?
+let moviesIncludesKeywordsCounter = 0;
+const numberOfMovieContainingKeywords = movies.forEach((movie) => {
+  const keywords = ["Surfer", "Alien", "Benjamin"];
+  const movieTitle = movie.title.toLowerCase();
+  for (let i = 0; i < keywords.length; i++) {
+    const keyword = keywords[i].toLowerCase();
+    if (movieTitle.includes(keyword)) {
+      ++moviesIncludesKeywordsCounter;
+    }
+  }
+});
+console.log(
+  `${moviesIncludesKeywordsCounter} titles of movies includes the keywords`
+);
+
+// Create an array of movies where a word in the title is duplicated.
+//     Fx "Star Wars: The Clone Wars" the word Wars is duplicated.
+//     Here are some madeup examples of movies with duplicated words in the title:
+// "The three men and the pistol", "Chase three - The final chase"
+const arrDupMovies = [];
+const moviesWithDuplicatedWordInTitle = movies.forEach((movie) => {
+  const arrTitleWords = movie.title.split(" ");
+  arrTitleWords.sort((a, b) => {
+    if (a === b) {
+      arrDupMovies.push(movie);
+    }
+  });
+
+  //   for (const word of arrTitleWords) {
+  //     const testArr = [];
+  //     test;
+  //     if (arrTitleWords.includes(word)) {
+  //       arrDupMovies.push(movie);
+  //     }
+  //   }
+});
+
+console.log(arrDupMovies);
+
+// Calculate the average rating of all the movies using reduce. Optional
+function getAverageMoviesRating(params) {
+  const averageMoviesRating = movies.reduce(
+    (total, movie) => total + movie.rating,
+    0
+  );
+  return averageMoviesRating / movies.length;
+}
+
+console.log(getAverageMoviesRating(movies));
