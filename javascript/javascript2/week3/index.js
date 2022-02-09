4fa3e72993169d240d0ef188bd76ec37b1098d76
BoroDinS <sergey.borodin.lg@gmail.com>
2022-02-09 17:19:51 +0100

    Function as a variable. added first exercise

--- javascript/javascript2/week3/index.js
+++ javascript/javascript2/week3/index.js
@@ -105,3 +105,29 @@ jokeCreator(
       "Helvetica and Times New Roman walk into a bar.“Get out of here!” shouts the bartender. “We don’t serve your type.”"
     )
 );
+
+// Function as a variable
+// Create funtions that are used in these different ways:
+
+// Create an array with 3 items. All items should be functions.
+//Iterate through the array and call all the functions.
+// Create a function as a const and try creating a function normally.
+//Call both functions.Read up on this if you are interested:
+//https://stackoverflow.com/questions/1013385/what-is-the-difference-between-a-function-expression-vs-declaration-in-javascrip
+// Create an object that has a key whose value is a function. Try calling this function.
+
+const arrFn = [
+  function fn1() {
+    console.log("First function");
+  },
+  function fn1() {
+    console.log("Second function");
+  },
+  function fn1() {
+    console.log("Third function");
+  },
+];
+
+arrFn.forEach((fn) => {
+  fn();
+});
