2e82060212412593211bc9ae660552d935762fc4
BoroDinS <sergey.borodin.lg@gmail.com>
2022-03-06 10:13:55 +0100

    repo exersice done

---
+++ javascript/javascript3/week3/main.js
@@ -0,0 +1,45 @@
+Promise.all([
+  fetch("https://api.github.com/search/repositories?q=user:oleksandra-lavryk"),
+  fetch("https://api.github.com/search/repositories?q=user:rismita87"),
+  fetch("https://api.github.com/search/repositories?q=user:veronicaburduzhan"),
+])
+  .then((responses) => {
+    console.log(responses);
+    return Promise.all(responses.map((response) => response.json()));
+  })
+  .then((data) => {
+    console.log(data);
+    renderData(data);
+  });
+
+function renderData(data) {
+  data.forEach((account) => {
+    const listOfRepos = document.getElementById("listOfRepos");
+    const itemRepo = document.createElement("li");
+    const userName = document.createElement("h3");
+    userName.textContent = `${account.items[0].owner.login}s repository`;
+    itemRepo.appendChild(userName);
+    for (let i = 0; i < account.items.length; i++) {
+      const repo = account.items[i];
+      console.log(repo.git_url);
+      const repoName = document.createElement("h4");
+      repoName.textContent = repo.name;
+      itemRepo.appendChild(repoName);
+      const link = document.createElement("a");
+      link.textContent = repo.git_url;
+
+      itemRepo.appendChild(link);
+    }
+
+    // if (repo.items.length) {
+    //   repo.items.forEach((repo) => {
+    //     const repoName = document.createElement("p");
+    //     repoName.textContent = repo.items[0].name;
+
+    //     itemRepo.appendChild(repoName);
+    //
+    //   });
+    // }
+    listOfRepos.appendChild(itemRepo);
+  });
+}
