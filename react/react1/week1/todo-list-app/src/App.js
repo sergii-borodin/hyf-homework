22e8d4ce32c9247766be770d6e8f0bf16948eb17
BoroDinS <sergey.borodin.lg@gmail.com>
2022-05-03 10:31:59 +0200

    React homework week1 done

---
+++ react/react1/week1/todo-list-app/src/App.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+import "./App.css";
+import todoList from "./todoList.json";
+import TodoListTitle from "./todoListTitle";
+import TodolistItem from "./todoListItem";
+
+
+function App() {
+  return (
+    <div className="App">
+      {<TodoListTitle/>}
+      {todoList.map((task) => {
+        return (
+          <TodolistItem key={task.title} title={task.title} deadline={task.deadline} />
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react1/week1/todo-list-app/src/todoListTitle.js
@@ -0,0 +1,9 @@
+ function TodoListTitle() {
+    return (
+        <h2>
+            Todo List
+        </h2>
+    )
+} 
+
+export default TodoListTitle;
